cbe2a2ad3247204df00c9e4defee51397a49ebf8
rakeshvali117 <rakeshvali117@gmail.com>
2019-10-08 22:20:15 +0530

    initial commit
    stable build

---
+++ Assets/Scripts/BobBehaviour.cs
@@ -0,0 +1,94 @@
+﻿/**
+ * Author:  Rakesh Kumar Vali
+ * Created: 05.06.2019
+ * Summary: Bob's functionality will be taken care by this class.
+ **/
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobBehaviour : MonoBehaviour
+{
+    public Transform    waypointObject;                     // Waypoint parent object reference 
+    public GameObject   damageEffect, climaxEffect;         // Bob's Particle effects 
+    public AudioClip    damageAudio, climaxAudio;           // Bob's Audio clips
+    public AudioSource  audioSource;                        // Audio source to be used by BoB
+
+    private int             wayPointIndex = 1;              // Waypoint index to access the waypoints            
+    private List<Vector3>   wayPoints;                      // List of waypoints to be used by Bob
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateWayPoints();
+    }
+
+    /// <summary>
+    /// Populates waypoints from referenced waypoint parent 
+    /// </summary>
+    void GenerateWayPoints()
+    {
+        wayPoints = new List<Vector3>();
+
+        foreach(Transform wpObj in waypointObject)
+        {
+            wayPoints.Add(wpObj.position);                  // Populates waypoints position from childs of waypoint parent
+        }
+
+        this.transform.position = wayPoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToWaypoint();
+    }
+
+    /// <summary>
+    /// Moves Bob to respective waypoints in the scene
+    /// </summary>
+    void MoveToWaypoint()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, wayPoints[wayPointIndex], Time.deltaTime * 10);      
+
+        if(Vector3.Distance(this.transform.position, wayPoints[wayPointIndex]) <= 0f)       // Checks if Bob reached the target waypoint
+        {
+            wayPointIndex++;                                                                // Access next waypoint
+            if(wayPointIndex > (wayPoints.Count - 1))
+            {
+                //Particle effects and sound effects
+                EndEffect(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Effects and audio that needs to be played based on whether Bob hits obstacle or finishes waypoints
+    /// </summary>
+    /// <param name="damaged"> Whether bob is damaged by obstacle</param>
+    void EndEffect(bool damaged)
+    {
+        if(damaged)
+        {
+            Instantiate(damageEffect, this.transform.position, Quaternion.identity);
+            audioSource.clip = damageAudio;
+        }
+        else
+        {
+            Instantiate(climaxEffect, this.transform.position, Quaternion.identity);
+            audioSource.clip = climaxAudio;
+        }
+        audioSource.Play();
+        Destroy(this.gameObject);
+    }
+
+    //Collision check
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "block")                // If Bob collides with obstacle
+        {
+            EndEffect(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,45 @@
+﻿/**
+ * Author:  Rakesh Kumar Vali
+ * Created: 05.06.2019
+ * Summary: Generates obstacles randomly within the floor space.
+ **/
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public int          noOfObstacles;              // No of obstacles to be generated
+    public GameObject   obstacle;                   // Prefab of the obstacle
+
+    private Dictionary<Vector3, GameObject> obstacles;
+
+    /// <summary>
+    /// Starts the behaviour
+    /// </summary>
+    void Start()
+    {
+        GenerateObstacles();            
+    }
+
+    /// <summary>
+    /// Generates Obstacles based on the floor size 
+    /// </summary>
+    void GenerateObstacles()
+    {
+        obstacles = new Dictionary<Vector3, GameObject>();
+
+        for (int objCount = 0; objCount < noOfObstacles; objCount++)
+        {
+            Vector3 randPos;
+            do
+            {
+                randPos = new Vector3(Random.Range(-60f, 60f), 1f, Random.Range(-57f, 62f));        // Random position for the obstacle
+            } while (obstacles.ContainsKey(randPos));                                               // Checks if there is already an obstacle in the position generated.
+
+            var obj = Instantiate(obstacle, randPos, Quaternion.identity);                          // Instantiates an obstacle and adds it as a child this gameobject
+            obj.transform.parent = this.transform;
+            obstacles.Add(randPos, obj);                                                            // Adds this position and object to dictionary.
+        }
+    }
+
+}
